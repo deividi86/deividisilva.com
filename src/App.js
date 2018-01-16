397c449a9e32299bc94002a6c97f7b2c796eba75
deividi86 <deividi@gmail.com>
2018-01-16 21:53:38 -0200

    remove boilerplate, use create react app with yarn

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Deividi Silva</h1>
+        </header>
+        <p className="App-intro">
+          Hi! My name is Deividi Silva and I'm a Full Stack Software Engineer.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
