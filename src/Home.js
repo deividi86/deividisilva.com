a783b4b5bbdc4713e56db4c687bfe58b3bff5513
deividi86 <deividi@gmail.com>
2018-02-18 11:01:27 -0300

    ligth background

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,13 @@ import './Home.css'
 
 export default function Home() {
     return (
-        <section className="container">      
+        <section className="container">                  
             <img src="./headshot.gif" alt="Deividi Silva" className="photo"/>
             <p className="who">
-            My name is Deividi Silva and I'm a software engineer in Brazil.
+            Hi! I'm Deividi Silva and I'm a software engineer in Brazil.
             </p>
-            <p className="why">
-            I'm passionate about creating software solutions that help people's life.
-            </p>
-            <p className="where">
-            I'm currently working for Whirpool S.A - Embraco compressors on an <a href="http://www.diili.com.br">awesome IoT solution</a>.       
+            <p className="what">
+            I love creating software solutions that help people's life. By day I'm currently working for Whirpool S.A - Embraco compressors on an <a href="http://www.diili.com.br">awesome IoT solution</a>.       
             </p>
         </section>
     )
