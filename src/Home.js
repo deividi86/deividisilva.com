76e6981f4b83fce8ef87c6111dd68999ce337bda
deividi86 <deividi@gmail.com>
2018-02-18 21:01:30 -0300

    resposiveness

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,9 @@ import './Home.css'
 export default function Home() {
     return (
         <section className="container">                  
-            <img src="./headshot.gif" alt="Deividi Silva" className="photo"/>
+            <img src="./headshot.jpg" alt="Deividi Silva" className="photo"/>
             <p className="who">
-            Hi! I'm Deividi Silva and I'm a software engineer in Brazil.
+            Hi! I'm Deividi Silva and I'm a software engineer from Brazil.
             </p>
             <p className="what">
             I love creating software solutions that help people's life. By day I'm currently working for Whirpool S.A - Embraco compressors on an <a href="http://www.diili.com.br">awesome IoT solution</a>.       
