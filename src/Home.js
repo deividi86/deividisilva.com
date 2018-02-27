78d3b5deead8dbebb3dd34acc832a47e4bffa0d3
Deividi Silva <deividi@gmail.com>
2018-02-27 11:07:16 -0300

    improve accessibility

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,19 @@ export default function Home() {
     return (
         <section className="container">
             <img src="/headshot.jpeg" alt="Deividi Silva" className="photo"/>
-            <div className="social">             
-                <a href="https://github.com/deividi86" className="fa fa-github"></a>
-                <a href="https://www.linkedin.com/in/deividisilva/" className="fa fa-linkedin-square"></a>
-                <a href="https://stackoverflow.com/users/2146559/deividi-silva" className="fa fa-stack-overflow"></a>
-                <a href="mailto:deividi@gmail.com?Subject=Website%20contact" target="_top" className="fa fa-envelope"></a>
+            <div className="social">
+                <a href="https://github.com/deividi86" aria-label="Github profile">
+                    <i className="fa fa-github" aria-hidden></i>
+                </a>
+                <a href="https://www.linkedin.com/in/deividisilva/" aria-label="Linkedin profile">
+                    <i className="fa fa-linkedin-square" aria-hidden></i>
+                </a>
+                <a href="https://stackoverflow.com/users/2146559/deividi-silva" aria-label="StackOverflow profile">
+                    <i className="fa fa-stack-overflow" aria-hidden></i>
+                </a>
+                <a href="mailto:deividi@gmail.com?Subject=Website%20contact" target="_top" aria-label="Send me an email to deividi@gmail.com">
+                    <i className="fa fa-envelope" aria-hidden></i>
+                </a>
             </div>
             <div className="professional">
                 <a href="https://www.certmetrics.com/amazon/public/badge.aspx?i=1&t=c&d=2017-05-10&ci=AWS00257056" alt="AWS certified architect"><img src="/architect.png" alt="I am an AWS certified architect" /></a>
